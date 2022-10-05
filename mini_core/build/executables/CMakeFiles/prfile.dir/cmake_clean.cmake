cb0d21f0ff5406c42f8efa6436e637c581e69663
Diana Turconi Bubenek <bubenekturconi.1529386@studenti.uniroma1.it>
2022-10-06 01:57:44 +0200

    homework

---
+++ mini_core/build/executables/CMakeFiles/prfile.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prfile.dir/many_object_in_stack_example.cpp.o"
+  "prfile"
+  "prfile.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prfile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
